5c9dc24cc2db8c6551444ab59a93fa51a905f5e0
gmunglani <gmunglani@gmail.com>
2018-06-01 17:51:40 +0200

    Object oriented structure in place

--- functions.py
+++ functions.py
@@ -1,43 +1,41 @@
-import cv2
 import numpy as np
 import pylab
-import logging
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.animation as animation
 import mpl_toolkits.mplot3d.axes3d as p3
 
 # Create animation of background subtraction
-def analysis(val, X1, Y1, X, Y, im_medianf, im_backf, im_unbleachf, varnf, maskf, signalf, labels1Df, numi, work_path, fname):
+def analysis_plot(X1, Y1, stack, work_out_path):
     """This functions plots the background analysis animation"""
-    def data(i, X, Y, line):
+    def data(i, stack, line):
         ax1.clear()
-        line1 = ax1.plot_surface(X, Y, im_medianf[:, :, i], cmap=cm.bwr, linewidth=0, antialiased=False)
-        ax1.set_title("{} Frame: {}".format(val, i + 1))
-        ax1.set_zlim(0, np.amax(im_medianf))
+        line1 = ax1.plot_surface(stack.X, stack.Y, stack.im_medianf[:, :, i], cmap=cm.bwr, linewidth=0, antialiased=False)
+        ax1.set_title("{} Frame: {}".format(stack.val, i + 1))
+        ax1.set_zlim(0, np.amax(stack.im_medianf))
         ax1.grid(False)
         ax1.set_xticklabels([])
         ax1.set_yticklabels([])
 
         ax2.clear()
-        minmax = np.amax(np.ravel(im_backf[:,:,i])) - np.amin(np.ravel(im_backf[:,:,i]))
-        line2 = ax2.plot_surface(X, Y, im_backf[:, :, i], cmap=cm.bwr, linewidth=0, antialiased=False)
+        minmax = np.ptp(np.ravel(stack.im_backf[:,:,i]))
+        line2 = ax2.plot_surface(stack.X, stack.Y, stack.im_backf[:, :, i], cmap=cm.bwr, linewidth=0, antialiased=False)
         ax2.set_title("Min to Max: {}".format(minmax))
-        ax2.set_zlim(0, np.amax(im_medianf))
+        ax2.set_zlim(0, np.amax(stack.im_medianf))
         ax2.grid(False)
         ax2.set_xticklabels([])
         ax2.set_yticklabels([])
 
         ax3.clear()
-        line3 = ax3.plot_surface(X1, Y1, im_unbleachf[i, :, :], cmap=cm.bwr, linewidth=0, antialiased=False)
-        ax3.set_title("Number of Tiles: {}".format(labels1Df[i].size))
-        ax3.set_zlim(0, np.amax(im_medianf))
+        line3 = ax3.plot_surface(X1, Y1, stack.im_framef[:, :, i], cmap=cm.bwr, linewidth=0, antialiased=False)
+        ax3.set_title("Number of Tiles: {}".format(stack.labelsf[:,i].size))
+        ax3.set_zlim(0, np.amax(stack.im_medianf))
         ax3.grid(False)
         ax3.set_xticklabels([])
         ax3.set_yticklabels([])
 
         ax4.clear()
-        ax4.set_title("Number of Tiles with Signal: {}".format(signalf[i]))
+        ax4.set_title("Number of Tiles with Signal: {}".format(-np.sum(stack.labelsf[:,i])))
         ax4.set_xlim(0, 1)
         ax4.set_ylim(0, 1)
         ax4.set_zlim(0, 1)
@@ -45,9 +43,9 @@ def analysis(val, X1, Y1, X, Y, im_medianf, im_backf, im_unbleachf, varnf, maskf
         ax4.set_xlabel('Variance', labelpad=10)
         ax4.set_ylabel('Skewness', labelpad=10)
         ax4.set_zlabel('Median', labelpad=10)
-        varn = varnf[:,:,i]
-        xyz = varn[maskf[i]]
-        xyz2 = varn[[not i for i in maskf[i]]]
+        varn = stack.propf[:,:,i]
+        xyz = varn[stack.maskf[:,i]]
+        xyz2 = varn[[not i for i in stack.maskf[:,i]]]
         line4 = ax4.scatter(xyz2[:, 0], xyz2[:, 1], xyz2[:, 3], c='blue')
         line4 = ax4.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 3], c='red', s=80)
 
@@ -66,15 +64,15 @@ def analysis(val, X1, Y1, X, Y, im_medianf, im_backf, im_unbleachf, varnf, maskf
     ax3.view_init(elev=15., azim=30.)
     ax4.view_init(elev=30., azim=210.)
 
-    line1 = ax1.plot_surface(X,Y,im_medianf[:,:,0],cmap=cm.bwr)
-    line2 = ax2.plot_surface(X,Y,np.ones((X.shape[0],X.shape[1])),cmap=cm.bwr)
-    line3 = ax3.plot_surface(X1,Y1,im_unbleachf[0,:,:],cmap=cm.bwr)
+    line1 = ax1.plot_surface(stack.X,stack.Y,stack.im_medianf[:,:,0],cmap=cm.bwr)
+    line2 = ax2.plot_surface(stack.X,stack.Y,np.ones((stack.width,stack.height)),cmap=cm.bwr)
+    line3 = ax3.plot_surface(X1,Y1,stack.im_framef[:,:,0],cmap=cm.bwr)
     line4 = ax4.scatter(10, 10, 10, c='red')
 
     line = [line1, line2, line3, line4]
 
     # Set up animation
-    anim = animation.FuncAnimation(fig, data, fargs=(X,Y,line),frames=numi, interval=20, blit=False, repeat_delay=1000)
+    anim = animation.FuncAnimation(fig, data, fargs=(stack,line),frames=stack.range, interval=20, blit=False, repeat_delay=1000)
 
     pylab.rc('font', family='serif', size=10)
     plt.show()
--- main_background_movies.py
+++ main_background_movies.py
@@ -17,248 +17,210 @@ import cv2
 import math
 import pims
 import os
-from functions import analysis, logit
 import h5py
+import logging
+from functions import analysis_plot
+from timeit import default_timer as timer
 
 # #############################################################################
 # Input parameters
 start = 1 # Start number of frames
-stop = 860 # End number of frames
-eps = [0.002]  # DBSCAN tolerance [higher epsilon = more background] - As low as possible
+stop = 10 # End number of frames
 specific = [] # Specific frames that need their own eps
+eps = [0.002]  # DBSCAN tolerance [higher epsilon = more background] - As low as possible
 
 # Options
-analysis_plot = True # Create animation of background subtraction
-h5_file = False # Create h5 file
+analysis = 2 # 1 - Create animation of background subtraction, 2 - Create h5 file, 3 - Create both
 
 # Path to files
 inp_path = '/home/gm/Documents/Work/Images/Ratio_tubes' # Path with the input Tiff files
 out_path = '/home/gm/Documents/Scripts/MATLAB/Tip_results' # Output path for further processing
 fname = 'YC18' # Sample name
-val = ['CFP'] # ENSURE THAT THE SIZE OF EPS AND VAL ARE THE SAME
+val = ['YFP'] # ENSURE THAT THE SIZE OF EPS AND VAL ARE THE SAME
 
-# Create folder if it does not exist
+# Create path/folder
 work_inp_path = inp_path + '/' + fname
 work_out_path = out_path+'/'+fname+'/'
 if not os.path.exists(work_out_path):
     os.makedirs(work_out_path)
+work_out_path += fname
 
-# Setup logging
-logger = stack.logit(work_out_path + fname + '_back.log')
+class frame(object):
+    def __init__(self,im_stack,count):
+        print('Image: ' + str(count + 1))
+        self.im_frame = np.asarray(im_stack[count])
+        self.count = count
 
-class stack(object):
-    def __init__(self,path,start=0,stop,val,eps):
-        im_path =  path + '_' + val[0] + '.tif'
+class stack(frame):
+    def __init__(self,work_inp_path,val,eps,*args):
+        self.val = val
+        self.eps = eps
+        self.saver = 0
+
+        if (len(args) == 2):
+            self.range = np.arange(args[0]-1,args[1])
+            self.type = False
+        else:
+            specific = args[0]
+            specific = [x - 1 for x in specific]
+            self.range = specific
+            self.type = True
+
+        im_path =  work_inp_path + '_' + self.val + '.tif'
         self.im_stack = pims.TiffStack_pil(im_path)
 
-        siz1,siz2 = self.im_stack.frame_shape
-        window = 40 if eps > 0.01 else 80
-        self.tile_dim = siz2/window
-        self.height = window
-        self.width = int(siz1/self.tile_dim)
+        self.siz1,self.siz2 = self.im_stack.frame_shape
+        window = 40 if self.eps > 0.01 else 80
+        self.dim = np.int16(self.siz2/window)
+        self.height = np.int16(window)
+        self.width = np.int16(self.siz1/self.dim)
 
-        self.X, self.Y = np.meshgrid(np.arange(self.height), np.arange(self.width))
-        X1, Y1 = np.meshgrid(np.arange(siz2), np.arange(siz1))
-        self.XY = np.column_stack((np.ravel(X),np.ravel(Y)))
+        self.X, self.Y = np.int16(np.meshgrid(np.arange(self.height), np.arange(self.width)))
+        self.XY = np.column_stack((np.ravel(self.X),np.ravel(self.Y)))
 
-        # Index at 0 instead if 1
-        self.range = np.arange(start-1,stop)
-
-    def specific(self,specific):
-        self.range = specific
-
-    def logit(self,name):
+    @classmethod
+    def logit(self,work_out_path):
         """ Logging input values """
         logger = logging.getLogger('back')
-        hdlr = logging.FileHandler(name)
+        hdlr = logging.FileHandler(work_out_path + '_back.log')
         formatter = logging.Formatter('%(asctime)s %(message)s')
         hdlr.setFormatter(formatter)
         logger.addHandler(hdlr)
         logger.setLevel(20)
 
-        return logger
+        self.logger = logger
 
-class frame(stack):
-    def __init__(self,count):
-        print('Image: ' + str(count + 1))
-        self.im_frame = np.asarray(self.im_stack[count])
+    def metric_prealloc(self):
+        length = len(self.range)
+        rows = self.height*self.width
+        self.im_medianf = np.empty((self.width,self.height,length),dtype=np.float32)
+        self.propf = np.empty((rows,4,length),dtype=np.float32)
+        self.maskf = np.empty((rows,length),dtype=np.bool)
+        self.labelsf = np.empty((rows,length),dtype=np.int8)
+        self.im_backf = np.empty((self.width,self.height,length),dtype=np.int16)
+        self.im_framef = np.empty((length,self.siz1,self.siz2),dtype=np.uint16)
 
-    def properties(self):
-        tile_prop = np.empty([self.width*self.height,4])
-        im_tile = np.reshape(self.im_frame,(self.width*self.height/self.tile_dim,self.tile_dim))
-        im_tile_split = np.split(im_tile,(self.tile_dim,self.tile_dim))
+    def properties(self,count):
+        self.ind = frame(self.im_stack,count)
+        tile_prop = np.empty([self.width*self.height,4],dtype=np.float32)
+        im_tile = np.reshape(self.ind.im_frame,(np.int32(self.siz1*self.siz2/self.dim),self.dim))
 
-        for i in (tile_prop.shape[0]):
-            tile_prop[i,0] = sp.stats.moment(im_tile_split[i],moment=2,axis=0)
-            tile_prop[i,1] = sp.stats.moment(im_tile_split[i],moment=3,axis=0)
-            tile_prop[i,2] = sp.stats.moment(im_tile_split[i],moment=4,axis=0)
-            tile_prop[i,3] = np.median(im_tile_split[i])
+        self.ind.im_tile_split = np.split(im_tile,np.arange(self.dim,im_tile.shape[0],self.dim))
 
-        self.im_median = np.copy(tile_prop[:,3])
+        for i in range(tile_prop.shape[0]):
+            im_tile_split_flat = np.ravel(self.ind.im_tile_split[i])
+            tile_prop[i,0] = sp.stats.moment(im_tile_split_flat,moment=2,axis=0)
+            tile_prop[i,1] = sp.stats.moment(im_tile_split_flat,moment=3,axis=0)
+            tile_prop[i,2] = sp.stats.moment(im_tile_split_flat,moment=4,axis=0)
+            tile_prop[i,3] = np.median(im_tile_split_flat)
+
+        self.ind.im_median = np.copy(tile_prop[:,3])
 
         tile_min = np.amin(tile_prop,axis=0)
         tile_ptp = np.ptp(tile_prop,axis=0)
 
-        for j in (tile_prop.shape[1]):
-            tile_prop = map(lambda x : (x - tile_min[j])/tile_ptp[j], tile_prop[j])
+        for j in range(tile_prop.shape[1]):
+            tile_prop[:,j] = map(lambda x : (x - tile_min[j])/tile_ptp[j], tile_prop[:,j])
 
-        self.tile_prop = tile_prop
+        self.ind.tile_prop = tile_prop
 
-    def clustering(self,eps):
-        db = DBSCAN(eps=eps, min_samples=int(self.height*1.25)).fit(self.tile_prop)
-        self.labels = db.labels_
+    def clustering(self):
+        db = DBSCAN(eps=self.eps, min_samples=int(self.height*1.25)).fit(self.ind.tile_prop)
+        self.ind.core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+        self.ind.core_samples_mask[db.core_sample_indices_] = True
+        self.ind.labels = np.int8(db.labels_)
 
-    def subtraction(self,val,count):
-        im_median_mask = np.multiply(self.im_median,(self.labels+1))
-        pos_front = np.where(im_median_mask==0)[0]
+    def subtraction(self):
+        im_median_mask = np.multiply(self.ind.im_median,(self.ind.labels+1))
+        pos_front = np.int16(np.where(im_median_mask==0)[0])
         XY_back = np.delete(self.XY, pos_front, axis=0)
         im_median_mask_back = np.delete(im_median_mask, pos_front, axis=0)
 
         try:
-            XY_interp_back = griddata(XY_back, im_median_mask_back, (self.X, self.Y), method='nearest')
+            self.ind.XY_interp_back = np.int16(griddata(XY_back, im_median_mask_back, (self.X, self.Y), method='nearest'))
         except:
-            XY_interp_back = np.zeros([self.width,self.height])
-            logger.error(val + '_eps: ' + str(eps) + ', frame: ' + str(count+1) + " (eps value too low)")
+            self.ind.XY_interp_back = np.zeros((self.width,self.height))
+            self.logger.error("".join(self.val,'_eps: ',str(self.eps),', frame: ',str(ind.count+1)," (eps value too low)"))
 
 
-for typ in range(len(val)):
-    print(val[typ])
-    if specific:
-        start = 0
-        stop = len(specific)
+        im_frame_split = np.empty((np.int32(self.siz1*self.siz2/self.dim),self.dim),dtype=np.int16)
+        for i,j in enumerate(self.ind.XY_interp_back.flat):
+            im_frame_split[i*self.dim:(i+1)*self.dim,0:self.dim] = np.subtract(self.ind.im_tile_split[i],j)
 
-    path = inp_path + '/' + fname + '_' + val[typ] + '.tif'
-    im = pims.TiffStack_pil(path)
+        self.ind.im_frame = np.reshape(im_frame_split,(self.siz1,self.siz2))
 
-    # Initialize variables
-    maskf = [0] * (stop-start)
-    n_clustersf = [0] * (stop-start)
-    labels1Df = [0] * (stop-start)
-    signalf = [0] * (stop-start)
+        low_values_flags = self.ind.im_frame < 0
+        self.ind.im_frame[low_values_flags] = 0
 
-    # Read in the image and convert to np array
-    for count in range(start,stop):
-        if specific:
-            print('Image: ' + str(specific[count]+1))
-            im2 = np.asarray(im[specific[count]])
+        self.ind.im_frame = np.float32(self.ind.im_frame)
+
+    def filter(self):
+        filtered = cv2.bilateralFilter(self.ind.im_frame, np.int16(math.ceil(9 * self.siz2 / 1280)), 30, 30)
+        self.ind.im_frame = np.uint16(filtered)
+
+    def metrics(self):
+        self.im_medianf[:,:,self.saver] = np.reshape(self.ind.im_median,(self.width,self.height))
+        self.im_backf[:,:,self.saver] = self.ind.XY_interp_back
+        self.im_framef[self.saver,:,:] = self.ind.im_frame
+        self.propf[:,:,self.saver] = self.ind.tile_prop
+        self.maskf[:,self.saver]  = self.ind.core_samples_mask.tolist()
+        self.labelsf[:,self.saver] = self.ind.labels
+        self.saver += 1
+
+    def logger_update(self,analysis):
+        al = True if analysis is not 2 else False
+        if (self.type):
+            self.logger.info(self.val + '_eps: ' + str(self.eps) + ', frames: ' + ",".join(
+                map(str, [x + 1 for x in self.range])) + ', save: ' + str(al))
         else:
-            print('Image: ' + str(count+1))
-            im2 = np.asarray(im[count])
+            self.logger.info(self.val + '_eps: ' + str(self.eps) + ', frames: ' + str(self.range[0]+1) + '-' + str(
+                self.range[-1]+1) + ', save: ' + str(al))
 
-        # Width and height of single frame
-        siz = im2.shape
-        window = 40 if eps[typ] > 0.01 else 80
-        tile = int(siz[1]/window)
-        height = int(siz[1]/tile)
-        width = int(siz[0]/tile)
-
-        # Initializing arrays
-        if (count == start):
-            im_medianf = np.empty([width, height, (stop-start)],dtype=np.float16)
-            im_backf = np.empty([width, height, (stop-start)],dtype=np.float16)
-            im_unbleachf = np.empty([(stop-start), siz[0], siz[1]],dtype=np.uint16)
-            varnf = np.empty([width*height, 4, (stop-start)],dtype=np.float16)
-
-            # Creates a grid for visualization
-            X, Y = np.meshgrid(np.arange(0,height), np.arange(0,width))
-            X1, Y1 = np.meshgrid(np.arange(0,siz[1]), np.arange(0,siz[0]))
-            X1D = np.ravel(X)
-            Y1D = np.ravel(Y)
-
-        # BACKGROUND SUBTRACTION
-        # Finds the median and higher moments in each window
-        var = np.empty([4])
-        im_median = np.zeros([width,height])
-        for x in range(0, width, 1):
-            for y in range(0, height, 1):
-                im_test = np.ravel(im2[x*tile:x*tile+tile,y*tile:y*tile+tile])
-                var = np.vstack((var,np.hstack((sp.stats.moment(im_test,moment=2,axis=0),sp.stats.moment(im_test,moment=3,axis=0),sp.stats.moment(im_test,moment=4,axis=0),np.median(np.ravel(im_test))))))
-                im_median[x,y] = np.median(im2[x*tile:x*tile+tile,y*tile:y*tile+tile])
-
-        # Normalize higher moments
-        var = np.delete(var,(0),axis=0)
-        varn = np.copy(var)
-        varn[:,0] = (varn[:,0]-np.amin(varn[:,0]))/(np.amax(varn[:,0])-np.amin(varn[:,0]))
-        varn[:,1] = (varn[:,1]-np.amin(varn[:,1]))/(np.amax(varn[:,1])-np.amin(varn[:,1]))
-        varn[:,2] = (varn[:,2]-np.amin(varn[:,2]))/(np.amax(varn[:,2])-np.amin(varn[:,2]))
-        varn[:,3] = (varn[:,3]-np.amin(varn[:,3]))/(np.amax(varn[:,3])-np.amin(varn[:,3]))
-
-        # DBSCAN clustering with output label matrix of the classifier
-        db = DBSCAN(eps=eps[typ], min_samples=int(height*1.25)).fit(varn)
-        core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-        core_samples_mask[db.core_sample_indices_] = True
-        labels1D = db.labels_
-        labels = np.reshape(labels1D,[width,height])
-
-        # Number of clusters in labels, ignoring noise if present.
-        n_clusters_ = len(set(labels1D)) - (1 if -1 in labels1D else 0)
-
-        # Mask by multiplying label matrix with median values (re-tile areas with no signal)
-        im_median_mask = np.multiply(im_median,(labels+1))
-        im_median_mask1D = np.ravel(im_median_mask)
-
-        # Re-tile positions with signal from grid and then interpolate using the background to estimate the background signal distribution
-        XY1D = np.column_stack((X1D,Y1D))
-        pos_front = np.where(im_median_mask1D==0)[0]
-        XY1D_back = np.delete(XY1D, pos_front, axis=0)
-        im_median_mask1D_back = np.delete(im_median_mask1D, pos_front, axis=0)
-
-        try:
-            XY_interp1D_back = griddata(XY1D_back, im_median_mask1D_back, (X, Y), method='nearest')
-        except:
-            logger.error(val[typ] + '_eps: ' + str(eps[typ]) + ', frame: ' + str(count+1) + " (eps value too low)")
-            raise ValueError('eps value is too low')
-
-        # Signal without background on the pixel level
-        im3 = np.copy(im2)
-        im3.setflags(write=1)
-        im3 = np.uint16(im3)
-        for x in range(0, width, 1):
-            for y in range(0, height, 1):
-                im3[x*tile:x*tile + tile, y*tile:y*tile + tile] = np.subtract(im3[x*tile:x*tile+tile,y*tile:y*tile+tile],int(XY_interp1D_back[x,y]))
-
-        # Re-tile negative values (array is circular)
-        high_values_flags = im3 > 65000
-        im3[high_values_flags] = 0
-
-        low_values_flags = im3 < 0
-        im3[low_values_flags] = 0
-
-        im3 = np.float32(im3)
-        im3 = cv2.bilateralFilter(im3,int(math.ceil(9 * siz[1] / 1280)),30,30)
-        im3 = np.uint16(im3)
-
-        # Updating arrays with properties from a single frame
-        im_medianf[:,:,count-start] = im_median
-        im_backf[:,:,count-start] = XY_interp1D_back
-        im_unbleachf[count-start,:,:] = im3
-        varnf[:,:,count-start] = varn
-        maskf[count-start] = core_samples_mask.tolist()
-        n_clustersf[count-start] = n_clusters_
-        labels1Df[count-start] = labels1D
-        signalf[count-start] = -np.sum(labels)
-
-    if (specific):
-        logger.info(val[typ] + '_eps: ' + str(eps[typ]) + ', frames: ' + ",".join(map(str, [x+1 for x in specific])) + ', save: ' + str(h5_file))
-    else:
-        logger.info(val[typ] + '_eps: ' + str(eps[typ]) + ', frames: ' + str(start+1) + '-' + str(stop) + ', save: ' + str(h5_file))
-
-    if (h5_file):
-        path = work_path + fname + '_back.h5'
+    def h5(self,work_out_path):
+        path = work_out_path + '_back.h5'
         f = h5py.File(path, 'a')
-        if specific:
-            orig = f[val[typ]]
-            for j in range(start,stop):
-                orig[specific[j]-1] = im_unbleachf[j]
+        if (self.type):
+            orig = f[self.val]
+            for i,j in enumerate(self.range):
+                orig[j] = self.im_framef[i]
         else:
-            k = val[typ] in f
+            k = self.val in f
             if (k):
                 del f[k]
-            dst = f.create_dataset(val[typ], data=im_unbleachf, shape=((stop - start), siz[0], siz[1]), dtype=np.uint16, compression='gzip')
+            dst = f.create_dataset(self.val, data=self.im_framef, shape=((len(self.range)), self.siz1, self.siz2),
+                                       dtype=np.uint16, compression='gzip')
 
-    if (analysis_plot):
-        analysis(val[typ], X1, Y1, X, Y, im_medianf, im_backf, im_unbleachf, varnf, maskf, signalf, labels1Df,
-                 (stop - start), work_path, fname)
-print("Fin")
+    def plot(self,work_out_path):
+            X1, Y1 = np.int16(np.meshgrid(np.arange(self.siz2), np.arange(self.siz1)))
+            analysis_plot(X1, Y1, self, work_out_path)
+
+startt = timer()
+for a,b in zip(val,eps):
+    if (specific):
+        all = stack(work_inp_path,a,b,specific)
+    else:
+        all = stack(work_inp_path,a,b,start,stop)
+
+    all.logit(work_out_path)
+    all.metric_prealloc()
+
+    for count in all.range:
+        all.properties(count)
+        all.clustering()
+        all.subtraction()
+        all.filter()
+        all.metrics()
+
+    all.logger_update(analysis)
+
+    if (analysis > 1):
+        all.h5(work_out_path)
+
+    if (analysis != 2):
+        all.plot(work_out_path)
+
+endt = timer()
+print(startt-endt)
+
 
 
